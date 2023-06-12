9e4d14cf15b0acb7049a1512f6059b679d6edcde
anshumanv <117473719+Geminitheman7@users.noreply.github.com>
2023-06-12 15:45:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//document.getElementById('teacher-login').addEventListener('click', function() {
+    // Teacher login button click event handling
+ //   alert('Teacher login clicked');
+ // });
+  
+ // document.getElementById('substitute-login').addEventListener('click', function() {
+    // Substitute login button click event handling
+ //   alert('Substitute login clicked');
+ // });
+  
